a9fa00a29c173190b5238433fcc7788c3d3677f5
Artur Maurício <arturmauricioss@outlook.com>
2026-02-23 11:06:26 -0300

    refatorando o código

--- script.js
+++ script.js
@@ -26,56 +26,84 @@ let musicas=[
 ]
 let indice_musica = 0;
 
-function tocando(){
-    html.classList.toggle('tocando')
+function alterar_dados(){
+        audio.src = musicas[indice_musica].audio;
+        capa.src = musicas[indice_musica].capa;
+        musica.innerHTML = musicas[indice_musica].titulo;
+        artista.innerHTML = musicas[indice_musica].autor;
+}
+
+function play_pause(){
     if(html.classList.contains('tocando')){
-        icone.setAttribute('src', './assets/pause.svg')
-        audio.play()
+        icone.setAttribute('src', './assets/pause.svg');
+        audio.play();
     }
     else{
-        icone.setAttribute('src', './assets/play.svg')
-        audio.pause()
+        icone.setAttribute('src', './assets/play.svg');
+        audio.pause();
     }
 }
+
+function tocando(){
+    html.classList.toggle('tocando');
+    play_pause();
+}
+
+
 function formatar_tempo(tempo){
 
-    let minutos = Math.floor(tempo/60)
-    let segundos = Math.floor(tempo%60) 
+    let minutos = Math.floor(tempo/60);
+    let segundos = Math.floor(tempo%60);
 
     if (segundos <10){
-        segundos = "0" + segundos
+        segundos = "0" + segundos;
     }
-    return minutos + ":" + segundos
+    return minutos + ":" + segundos;
 }
-tempo_total.innerHTML = formatar_tempo(audio.duration)   
+
+tempo_total.innerHTML = formatar_tempo(audio.duration);
 
 audio.addEventListener('loadedmetadata', () => {
-    tempo_total.innerHTML = formatar_tempo(audio.duration)    
+    tempo_total.innerHTML = formatar_tempo(audio.duration); 
 })
+
+function reset_bar(){
+    barra_progresso_atual.style.width = 0;
+    ponto_tempo.style.marginLeft = "-4px";
+}
 
 audio.addEventListener('timeupdate', () => {
     tempo_atual.innerHTML = formatar_tempo(audio.currentTime);
     progresso_do_audio = audio.currentTime/audio.duration;
     barra_progresso_atual.style.width = (progresso_do_audio*100)+ "%";
-    ponto_tempo.style.marginLeft = (progresso_do_audio*100-1)+ "=";
-
+    ponto_tempo.style.marginLeft = (progresso_do_audio*100-1)+ "%";
 });
 
 function tocar_proxima(){
     indice_musica++;
-    audio.currentTime = 0;
-    barra_progresso_atual.style.width = "0%";
-    ponto_tempo.style.marginLeft = "-4px";
     if (indice_musica >= musicas.length){
         indice_musica = 0;
     }
-    audio.src = musicas[indice_musica].audio;
-    capa.src = musicas[indice_musica].capa;
-    musica.innerHTML = musicas[indice_musica].titulo;
-    artista.innerHTML = musicas[indice_musica].autor;
-    icone.setAttribute('src', './assets/pause.svg');
-    audio.play();
+    alterar_dados();
+    reset_bar();  
+    play_pause();
 }
 audio.addEventListener('ended', () => {
     tocar_proxima();
 });
+
+function tocar_anterior_reset(){
+    if(audio.currentTime<5){
+        indice_musica--;
+        if (indice_musica <0){
+            num_musicas = musicas.length - 1;
+            indice_musica = num_musicas;
+        }
+        alterar_dados();
+        reset_bar();
+        play_pause();
+    }
+    else {
+        audio.currentTime = 0;
+    } 
+}
